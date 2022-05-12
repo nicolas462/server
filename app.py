19cd3018d9f794156554b46557daa146db02122e
Nicolás David Espejo Bernal <nespejobern@uniminuto.edu.co>
2022-05-12 17:46:38 -0500

    exception handling

--- app.py
+++ app.py
@@ -20,31 +20,34 @@ def index():
 
 @app.route('/send-poll', methods=['POST'])
 def get_poll_data():
-    currentDate = getCurrentDate()
-    content = request.json
-    schedulePoll = content['schedule']
-    class_nrc = content['nrc']
-    attendance_list = content['attendance']
-    #call class to know what class is (depends on day)
-    idClassSession = Class.getClassSessionByNrcAndDay(class_nrc, int(currentDate.weekday()) + 1)
-    endTimeClass = tz.localize(idClassSession[1])
-    #wait until class finishes
-    if endTimeClass.time() > currentDate.time() or not(schedulePoll):
-        #create historic class session
-        hCS = HistoricClassSession()
-        hCS.addHistoricClassSession(idClassSession[0], (f"{currentDate.year}-{currentDate.day}-{currentDate.day}"))
-        #get response
-        professorName = Professor.findProfessorNameByNrc(class_nrc)
-        matter = Class.getClassNameByNrc(class_nrc)
-        threads = list()
-        for idStudent in attendance_list.split(','):
-            whatsappNumber = Student.getWhatsappNumberById(idStudent)
-            poll = Poll(professorName, matter, whatsappNumber)
-            #function to wait
-            t = threading.Thread(target=asyncSendingPoll, args=(poll, hCS.id, idStudent, schedulePoll, endTimeClass, currentDate))
-            threads.append(t)
-            t.start()
-        return 'Request processed successfully.', 201
+    try:
+        currentDate = getCurrentDate()
+        content = request.json
+        schedulePoll = content['schedule']
+        class_nrc = content['nrc']
+        attendance_list = content['attendance']
+        #call class to know what class is (depends on day)
+        idClassSession = Class.getClassSessionByNrcAndDay(class_nrc, int(currentDate.weekday()) + 1)
+        endTimeClass = tz.localize(idClassSession[1])
+        #wait until class finishes
+        if endTimeClass.time() > currentDate.time() or not(schedulePoll):
+            #create historic class session
+            hCS = HistoricClassSession()
+            hCS.addHistoricClassSession(idClassSession[0], (f"{currentDate.year}-{currentDate.day}-{currentDate.day}"))
+            #get response
+            professorName = Professor.findProfessorNameByNrc(class_nrc)
+            matter = Class.getClassNameByNrc(class_nrc)
+            threads = list()
+            for idStudent in attendance_list.split(','):
+                whatsappNumber = Student.getWhatsappNumberById(idStudent)
+                poll = Poll(professorName, matter, whatsappNumber)
+                #function to wait
+                t = threading.Thread(target=asyncSendingPoll, args=(poll, hCS.id, idStudent, schedulePoll, endTimeClass, currentDate))
+                threads.append(t)
+                t.start()
+            return 'Request processed successfully.', 201
+    except Exception as e:
+        return e, 402
     else:
         return 'The class session has already ended.', 401
 
